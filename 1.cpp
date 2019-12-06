3c15fd0160b21f3fea2367f969b8120b533e3dab
Alexandr Isaenkov <48714228+UnicornMane@users.noreply.github.com>
2019-12-06 16:03:46 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+double f(double& x)
+{
+	return pow(x, 11) + 56 * x + 7;
+}
+
+auto main() -> int
+{
+	const double INF = 1e6;
+	double r = 1e6, l = -INF;
+
+	for (int i = 0; i < 100000; i++)
+	{
+		double m = (r + l) / 2;
+		if (f(m) > 0)
+		{
+			r = m;
+		}
+		else
+		{
+			l = m;
+		}
+	}
+	cout << setprecision(56) << fixed << l;
+}
