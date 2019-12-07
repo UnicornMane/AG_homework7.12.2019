75e0ba16b2c1275b5b2ad7916c30d8fdd9f7e32d
Alexandr Isaenkov <48714228+UnicornMane@users.noreply.github.com>
2019-12-07 21:56:06 +0300

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+//напишите программу для вычисления ЧФ с помощью разделяй и властвуй.
+
+int fib(int n)
+{
+	if (n == 1 || n == 2)
+		return 1;
+	if (n % 2 == 0)
+	{
+		return pow(fib(n / 2 + 1), 2) - pow(fib(n / 2 - 1), 2);
+	}
+		return pow(fib((n - 1) / 2), 2) + pow(fib((n - 1) / 2 + 1), 2);
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << fib(n);
+}
